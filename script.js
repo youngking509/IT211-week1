0f4d8e4e06e4729ea2396b3aa043061f07a2ac80
Kingzy Valcourt <kingzyv14@gmail.com>
2023-06-28 23:55:16 -0700

    add intro

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
+
+
+let hw = document.getElementById("intro")
+
+
 let firstName = "Kingzy";
 let lastName = "Valcourt";
-let yearOfStudy = 2
-let goal = "Be a Software Engineer"
+let yearOfStudy = 4 + " " + "months"
+let goal = "start a career as a Software Engineer"
+
+hw.innerHTML = `My name is ${firstName} ${lastName}. I have been at SCC for ${yearOfStudy} and plan to ${goal} after completing my courses. `;
